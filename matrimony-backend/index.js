a2797237de43c6db4643b40ea3b4c708c042ad0a
Aswinrajr <aswinrajr07@gmail.com>
2025-07-03 23:03:13 +0530

    logo changed

--- matrimony-backend/index.js
+++ matrimony-backend/index.js
@@ -16,7 +16,7 @@ dbConnect();
 
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174","https://matrimony-app-318k.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174","https://matrimony-app-6wx5.vercel.app"];
 
 const corsOptions = {
   origin: function (origin, callback) {
